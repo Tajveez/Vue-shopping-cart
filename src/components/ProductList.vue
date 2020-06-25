53f751f0b08adae60dab650ac2b8498c51fad9fb
Tajveez <tajveez786@gmail.com>
2020-06-25 23:17:38 +0500

    adding code files

---
+++ src/components/ProductList.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <ul>
+      <li v-for="product in products" :key="product.key">{{product.title}} - {{product.price}}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import shop from "@/api/shop";
+export default {
+  data() {
+    return {
+      products: []
+    };
+  },
+  created() {
+    shop.getProducts(products => {
+      this.products = products;
+    });
+  }
+};
+</script>
+
+<style>
+</style>
